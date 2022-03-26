0fa88da1e688d95cf48f5016ce9703ff216229f1
Divik Babbar <91770907+Code19Master@users.noreply.github.com>
2022-03-26 15:18:03 +0530

    My FIrst Commit

---
+++ .config/BetterDiscord/plugins/SendEmbeds.plugin.js
@@ -0,0 +1,244 @@
+/**
+ * @name SendEmbeds
+ * @author Fraserbc
+ * @version 2.1.0
+ * @description Allows you to send embeds
+ * @website https://github.com/Fraserbc
+ * @source https://github.com/Fraserbc/BetterDiscord-Embeds/blob/master/SendEmbeds.plugin.js
+ */
+
+ const config = {
+    info: {
+        name: 'SendEmbeds',
+        authors: [
+            {
+                name: 'Fraser Price',
+                github_username: 'Fraserbc'
+            }
+        ],
+        version: '2.1.0',
+        description: 'Allows you to send embeds',
+        github: 'https://github.com/Fraserbc/BetterDiscord-Embeds',
+        github_raw: 'https://raw.githubusercontent.com/Fraserbc/BetterDiscord-Embeds/master/SendEmbeds.plugin.js'
+    }
+};
+
+module.exports = class {
+    constructor() {
+        this._config = config;
+    }
+
+    getName() {
+        return config.info.name;
+    }
+
+    getAuthor() {
+        return config.info.authors.map(author => author.name).join(', ');
+    }
+
+    getDescription() {
+        return config.info.description;
+    }
+
+    getVersion() {
+        return config.info.version;
+    }
+
+	load() {}
+	unload() {}
+	start() { this.attachHandler(); }
+	onSwitch() { this.attachHandler(); }
+	stop() {
+		let el = document.querySelectorAll('form[class^="form-');
+		if (el.length == 0) return;
+
+		// Remove handlers and injected script
+		el[0].removeEventListener('keydown', this.handler);
+	}
+
+	attachHandler() {
+		this.handler = this.handleKeypress.bind(this);
+		let el = document.querySelectorAll('form[class^="form-');
+		if (el.length == 0) return;
+
+		// Bind the handler
+		el[0].addEventListener('keydown', this.handler, false);
+	}
+
+	// Function that sends the embed
+	sendEmbed(embed) {
+		// Get the ID of the channel we want to send the embed
+		let channelID = BdApi.findModuleByProps('getLastSelectedChannelId').getChannelId();
+
+		// Create the message
+		let MessageQueue = BdApi.findModuleByProps('enqueue');
+		let MessageParser = BdApi.findModuleByProps('createBotMessage');
+
+		let msg = MessageParser.createBotMessage(channelID, '');
+
+		// Send the message
+		MessageQueue.enqueue({
+			type: 0,
+			message: {
+				channelId: channelID,
+				content: '',
+				tts: false,
+				nonce: msg.id,
+				embed: embed
+			}
+		}, r => {
+			return;
+		});
+	}
+
+	// Handling the user input
+	handleKeypress(e) {
+		var code = e.keyCode || e.which;
+
+		if (code !== 13) {
+			return;
+		}
+
+		if (e.shiftKey) {
+			return;
+		}
+
+		// Split a string on only the first delimeter
+		function splitSingle(str, delimeter) {
+			let part1 = str.substr(0, str.indexOf(delimeter));
+			let part2 = str.substr(str.indexOf(delimeter) + 1);
+
+			return [part1, part2]
+		};
+
+		// Get the deepest child of a parent
+		function getDeepest(elem) {
+			if(elem.firstChild == null) {
+				return elem;
+			} else {
+				return getDeepest(elem.firstChild);
+			}
+		};
+
+		// Parse the text
+		let elements = Array.from(document.querySelectorAll('div[class^="textArea-')[0].children[0].children);
+		let text = '';
+		elements.forEach(function(l0) {
+			Array.from(l0.children).forEach(function(l1) {
+				Array.from(l1.children).forEach(function(elem) {
+					elem = getDeepest(elem);
+					if(elem.alt) {
+						text += elem.alt;
+					} else {
+						text += elem.textContent;
+					}
+				});
+			});
+			text += '\n';
+		});
+
+		if (!text.startsWith('/e')) {
+			return;
+		};
+
+		// Cancel the event so we can handle it ourselves
+		e.preventDefault();
+		e.stopPropagation();
+
+		// Strip and split the text
+		text = text.replace('/e ', '');
+		text = text.replace('\uFEFF', '');
+		text = text.replace(/\n\n/g, '\n');
+		text = text.split('\n');
+
+		// Create the embed
+		let fields = ['title', 'description', 'url', 'color', 'timestamp', 'footer_image', 'footer', 'thumbnail', 'image', 'author', 'author_url', 'author_icon'];
+		let embed = {};
+		let last_attrb = ''
+		for (var x = 0; x < text.length; x++) {
+			let line = text[x]
+			let split = splitSingle(line, ':');
+
+			// Check if it is an attribute or continuation of previous
+			if(fields.includes(split[0])) {
+				// Check if there is a leading ' '
+				if(split[1].startsWith(' ')) {
+					embed[split[0]] = split[1].slice(1);
+				} else {
+					embed[split[0]] = split[1];
+				}
+
+				// Store the last attribute to be set so we can have multi-line
+				last_attrb = split[0];
+			} else {
+				embed[last_attrb] += '\n' + line;
+			}
+		}
+
+		// Find the unused fields
+		let unused = [];
+		let keys = Object.keys(embed);
+		for (var x = 0; x < keys.length; x++) {
+			if (embed[keys[x]] == '') {
+				unused.push(keys[x]);
+			}
+		}
+
+		// Remove the unused fields
+		for (var x = 0; x < unused.length; x++) {
+			delete embed[unused[x]];
+		}
+
+		// Proccess color
+		embed.color = embed.color ? parseInt(embed.color.replace('#', ''), 16) : 0;
+
+		// Convert the embed to Discord's format
+		let discordEmbed = {
+			type: 'rich',
+			footer: { text: '' },
+			author: { name: '' }
+		}
+		keys = Object.keys(embed);
+		for (var x = 0; x < keys.length; x++) {
+			switch(keys[x]) {
+				case 'timestamp':
+					if (embed.timestamp.toLowerCase() == 'true') {
+						let timestamp = (new Date).toISOString();
+						discordEmbed.timestamp = timestamp;
+					}
+					break;
+				case 'footer_image':
+					discordEmbed.footer.icon_url = embed.footer_image;
+					break;
+				case 'footer':
+					discordEmbed.footer.text = embed.footer;
+					break;
+				case 'thumbnail':
+					discordEmbed.thumbnail = {};
+					discordEmbed.thumbnail.url = embed.thumbnail;
+					break;
+				case 'image':
+					discordEmbed.image = {};
+					discordEmbed.image.url = embed.image;
+					break;
+				case 'author':
+					discordEmbed.author.name = embed.author;
+					break;
+				case 'author_url':
+					discordEmbed.author.url = embed.author_url;
+					break;
+				case 'author_icon':
+					discordEmbed.author.icon_url = embed.author_icon;
+					break;
+				default:
+					discordEmbed[keys[x]] = embed[keys[x]];
+					break;
+			}
+		}
+
+		// Send the embed
+		this.sendEmbed(discordEmbed);
+
+		this.lastKey = 0;
+	}
+};
---
+++ .vscode/extensions/leonardssh.vscord-4.5.0/README.md
@@ -0,0 +1,122 @@
+<p align="center">
+  <a href="https://marketplace.visualstudio.com/items?itemName=LeonardSSH.vscord" target="_blank" rel="noopener noreferrer">
+    <img width="256" src="https://i.imgur.com/n7ieZfW.png" alt="VSCord Logo">
+  </a>
+</p>
+
+<br>
+
+<p>
+    <p align="center">
+        <a href="https://marketplace.visualstudio.com/items?itemName=LeonardSSH.vscord">
+            <img alt="Visual Studio Marketplace Version" src="https://img.shields.io/visual-studio-marketplace/v/LeonardSSH.vscord?label=Visual%20Studio%20Marketplace" />
+        </a>
+        <a href="https://marketplace.visualstudio.com/items?itemName=LeonardSSH.vscord">
+            <img alt="Visual Studio Marketplace Downloads" src="https://img.shields.io/visual-studio-marketplace/d/LeonardSSH.vscord" />
+        </a>
+        <a href="https://marketplace.visualstudio.com/items?itemName=LeonardSSH.vscord">
+            <img alt="Visual Studio Marketplace Installs" src="https://img.shields.io/visual-studio-marketplace/i/LeonardSSH.vscord" />
+        </a>
+        <a href="https://marketplace.visualstudio.com/items?itemName=LeonardSSH.vscord">
+            <img alt="Visual Studio Marketplace Rating" src="https://vsmarketplacebadge.apphb.com/rating-short/LeonardSSH.vscord.svg" />
+        </a>
+		<a href="https://github.com/LeonardSSH/vscord/actions/workflows/cd-vs-marketplace.yml">
+			<img alt="CD - Visual Studio Marketplace" src="https://img.shields.io/github/workflow/status/leonardssh/vscord/CD%20-%20Visual%20Studio%20Marketplace?label=CD%20-%20Visual%20Studio%20Marketplace">
+		</a>
+    </p>
+    <p align="center">
+		<a href="https://open-vsx.org/extension/LeonardSSH/vscord">
+			<img alt="Open VSX Version" src="https://img.shields.io/open-vsx/v/LeonardSSH/vscord?label=OpenVSX%20Marketplace">
+		</a>
+		<a href="https://open-vsx.org/extension/LeonardSSH/vscord">
+			<img alt="Open VSX Downloads" src="https://img.shields.io/open-vsx/dt/LeonardSSH/vscord">
+		</a>
+		<a href="https://open-vsx.org/extension/LeonardSSH/vscord">
+			<img alt="Open VSX Rating" src="https://img.shields.io/open-vsx/rating/LeonardSSH/vscord">
+		</a>
+		<a href="https://github.com/LeonardSSH/vscord/actions/workflows/cd-open-vsx.yml">
+			<img alt="CD - Open VSX Registry" src="https://img.shields.io/github/workflow/status/leonardssh/vscord/CD%20-%20Open%20VSX%20Registry?label=CD%20-%20Open%20VSX%20Registry">
+		</a>
+	</p>
+    <p align="center">
+        <a href="https://gitter.im/LeonardSSH/vscord-support?utm_source=badge&utm_medium=badge&utm_campaign=pr-badge">
+            <img src="https://img.shields.io/badge/gitter-support chat-green?color=40aa8b" />
+        </a>
+    </p>
+</p>
+
+<br>
+
+# Discord Rich Presence
+
+Remember to 🌟 this GitHub if you 💖 it.
+
+> Fully customizable VS Code extension to get Discord Rich Presence integration
+
+## 📌 Features
+
+-   Switch between 2 IDE names (`Code` and `Visual Studio Code`) [(preview)](https://streamable.com/apjd4g)
+-   Highly customizable in using the extension settings
+-   Shows what you're working on in Visual Studio Code
+-   Shows the amount of problems in your workspace
+-   Shows the number of lines in your file and which line you're editing
+-   Shows the git repository and branch you are working on
+-   Respects Discords 15sec limit when it comes to updating your status
+-   Support for over 130+ of the most popular languages
+-   Enable/Disable RPC for individual workspaces
+-   Custom string support
+-   Stable or Insiders build detection
+-   Debug mode detection
+-   Easily manually reconnect to Discord Gateway
+-   Idle indication when you tab out for a while
+-   Button that sends you to the GitHub repository
+
+> All texts are fully customizable using variables and a multitude of config options
+
+<!--You know other people can read this too?-->
+
+![1](https://i.imgur.com/LaB4TqM.png)
+![2](https://i.imgur.com/yTFIFiK.png)
+![3](https://i.imgur.com/5OOkKUW.png)
+
+## 📥 Installation
+
+### Prerequisites
+
+**[Visual Studio Code](https://code.visualstudio.com/)** or **[Visual Studio Code Insiders](https://code.visualstudio.com/insiders/)**
+
+### Install
+
+Launch VS Code Quick Open (Ctrl+P), paste the following command, and press enter.
+
+```
+ext install LeonardSSH.vscord
+```
+
+**OR**
+
+Use the **[Extension Marketplace](https://code.visualstudio.com/docs/editor/extension-gallery)**
+
+![a4](https://i.imgur.com/qMzox38.gif)
+
+## 👨‍💻 Contributing
+
+To contribute to this repository, feel free to create a new fork of the repository and submit a pull request.
+
+1. Fork / Clone and select the `main` branch.
+2. Create a new branch in your fork.
+3. Make your changes.
+4. Commit your changes, and push them.
+5. Submit a Pull Request [here](https://github.com/LeonardSSH/vscord/pulls)!
+
+## 🎉 Thanks
+
+-   [discordjs](https://github.com/discordjs/) - Creator of Discord RPC Client
+-   [iCrawl](https://github.com/iCrawl) - Creator of [discord-vscode](https://github.com/iCrawl/discord-vscode)
+-   [Satoqz](https://github.com/Satoqz) - Creator of [vscode-discord](https://github.com/Satoqz/vscode-discord/)
+
+_Much of the code in this repository is based on [iCrawl/discord-vscode](https://github.com/iCrawl/discord-vscode) & [Satoqz/vscode-discord](https://github.com/Satoqz/vscode-discord). This extension would not exist without them._
+
+## 📋 License
+
+This project is licensed under the MIT License - see the [LICENSE](https://github.com/LeonardSSH/vscord/blob/HEAD/LICENSE) file for details.
